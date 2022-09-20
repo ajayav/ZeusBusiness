14fa90537ca24a7531ab8cebdface60c8192ce93
AJ <ajaywynd97@gmail.com>
2022-09-20 12:26:44 +0530

    Add project files.

---
+++ ZeusBusiness/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ZeusBusiness;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ZeusBusiness/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ZeusBusiness;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
